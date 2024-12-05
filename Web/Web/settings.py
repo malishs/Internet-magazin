b7330e2f8f698de1eb79ba0f24977d5a8fa99993
Your Name <you@example.com>
2024-12-05 16:41:47 +0300

    Я задолбался

--- Web/Web/settings.py
+++ Web/Web/settings.py
@@ -133,7 +133,7 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDUA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 SESSION_SAVE_EVERY_REQUEST = True
 
